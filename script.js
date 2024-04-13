ef9cf8c24482b3a14c8417eddd67e4175d0ae983
RohanScripts <shelarrohan1810@gmail.com>
2024-04-14 00:52:30 +0530

    Update script.js with user data and profile information

---
+++ script.js
@@ -0,0 +1,54 @@
+var users = [
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 4,
+    location: "Delhi, India",
+    name: "Harshita",
+    age: 23,
+    interests: ["music", "painting"],
+    bio: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    isFriend: null,
+  },
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 6,
+    location: "Ahmedabad, India",
+    name: "Riddhi",
+    age: 22,
+    interests: ["music", "painting"],
+    bio: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod",
+    isFriend: null,
+  },
+  {
+    profilePic:
+      "https://plus.unsplash.com/premium_photo-1674069720020-41e6dea32366?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 4,
+    location: "Delhi, India",
+    name: "Maitri",
+    age: 18,
+    interests: ["music", "painting"],
+    bio: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore",
+    isFriend: null,
+  },
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1519895609939-d2a6491c1196?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1614204424926-196a80bf0be8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 4,
+    location: "Delhi, India",
+    name: "juli",
+    age: 20,
+    interests: ["music", "painting"],
+    bio: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    isFriend: null,
+  },
+];
